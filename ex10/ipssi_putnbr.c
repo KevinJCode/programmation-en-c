1b31dc0576c2cbc003dc4b4ccf3e030dc41c9231
KevinjCode <megbart@msn.com>
2025-10-15 10:26:08 +0200

    mise a jour

--- ex10/ipssi_putnbr.c
+++ ex10/ipssi_putnbr.c
@@ -1,3 +1,32 @@
 #include <unistd.h>
 
-void	ipssi_putnbr(int nb)
+void	ipssi_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+
+
+void	 ipssi_putnbr(int nbr)
+{
+	if (nbr < 0)
+	{
+		ipssi_putchar('-');
+		ipssi_putchar(nbr);
+	}
+	else if (nbr > 9)
+	{
+		ipssi_putnbr(nbr / 10);
+		ipssi_putnbr(nbr % 10);
+	}
+	else
+	{
+		ipssi_putchar(nbr + '0');
+	}
+}
+
+
+int	main(void)
+{
+	ipssi_putnbr(-42);
+}
